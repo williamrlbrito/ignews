f45a27664ff8c166a5597c7b9f5649d0d839fa8d
William Brito <williamrldb@gmail.com>
2021-06-08 11:56:39 -0400

    --Listando posts em tela

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -3,8 +3,20 @@ import Head from 'next/head'
 import Prismic from '@prismicio/client'
 import { getPrismicClient } from '../../services/prismic';
 import styles from './styles.module.scss'
+import { RichText } from 'prismic-dom';
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+}
+
+interface PostsProps {
+  posts: Post[]
+}
+
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
@@ -13,21 +25,13 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="#">
-            <time>12 de marco de 2021</time>
-            <strong>Notas sobre sexo e catolicismo</strong>
-            <p>Não vou enrolar, vou direto ao ponto: o mundo está uma bagunça em  termos de ideias e valores e a maioria das pessoas (católicas ou não)  estão no bojo desse caos sem entender muito sobre quase nenhum assunto,  especialmente o sexo. Entre católicos, falar sobre isso ora vai para o  lado do tabu e puritanismo, ora vai para o lado da permissividade e  banalidade sexual. De um lado, há pessoas que chegaram ao casamento (e  estão nele) achando que sentir prazer na relação sexual é “um mal  necessário” e que procurar maneiras de melhorar a vida sexual pode ser  pecado. Do outro, há aqueles católicos que usam contraceptivos  artificiais e acham que assistir pornografia é normal. Falta equilíbrio e  bom senso. Falta doutrina!</p>
-          </a>
-          <a href="#">
-            <time>12 de marco de 2021</time>
-            <strong>Notas sobre sexo e catolicismo</strong>
-            <p>Não vou enrolar, vou direto ao ponto: o mundo está uma bagunça em  termos de ideias e valores e a maioria das pessoas (católicas ou não)  estão no bojo desse caos sem entender muito sobre quase nenhum assunto,  especialmente o sexo. Entre católicos, falar sobre isso ora vai para o  lado do tabu e puritanismo, ora vai para o lado da permissividade e  banalidade sexual. De um lado, há pessoas que chegaram ao casamento (e  estão nele) achando que sentir prazer na relação sexual é “um mal  necessário” e que procurar maneiras de melhorar a vida sexual pode ser  pecado. Do outro, há aqueles católicos que usam contraceptivos  artificiais e acham que assistir pornografia é normal. Falta equilíbrio e  bom senso. Falta doutrina!</p>
-          </a>
-          <a href="#">
-            <time>12 de marco de 2021</time>
-            <strong>Notas sobre sexo e catolicismo</strong>
-            <p>Não vou enrolar, vou direto ao ponto: o mundo está uma bagunça em  termos de ideias e valores e a maioria das pessoas (católicas ou não)  estão no bojo desse caos sem entender muito sobre quase nenhum assunto,  especialmente o sexo. Entre católicos, falar sobre isso ora vai para o  lado do tabu e puritanismo, ora vai para o lado da permissividade e  banalidade sexual. De um lado, há pessoas que chegaram ao casamento (e  estão nele) achando que sentir prazer na relação sexual é “um mal  necessário” e que procurar maneiras de melhorar a vida sexual pode ser  pecado. Do outro, há aqueles católicos que usam contraceptivos  artificiais e acham que assistir pornografia é normal. Falta equilíbrio e  bom senso. Falta doutrina!</p>
-          </a>
+          {posts.map(post=>(
+            <a key={post.slug} href="#">
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          ))}
         </div>
       </main>
     </>
@@ -43,11 +47,22 @@ export const getStaticProps: GetStaticProps = async () => {
     fetch: ['post.title', 'post.content']
   })
 
-  console.log(JSON.stringify(response, null, 2))
+  const posts = response.results.map(post => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR',{
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+      })
+    }
+  })
 
   return {
     props: {
-
+      posts
     }
   }
 }
