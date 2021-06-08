5eac8e8f277e9c040d97f8c656db78fc11399f6c
William Brito <williamrldb@gmail.com>
2021-06-07 20:57:47 -0400

    --Verificando usuário duplicado

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -12,18 +12,31 @@ export default NextAuth({
       scope: 'read:user'
     })
   ],
-  jwt:{
-    signingKey: process.env.SIGNING_KEY
-  },
   callbacks: {
     async signIn(user, account, profile){
       const {email} = user
 
       try {
         await fauna.query(
-          query.Create(
-            query.Collection('users'),
-            { data: { email } }
+          query.If(
+            query.Not(
+              query.Exists(
+                query.Match(
+                  query.Index('user_by_email'),
+                  query.Casefold(user.email)
+                )
+              )
+            ),
+            query.Create(
+              query.Collection('users'),
+              { data: { email } }
+            ),
+            query.Get(
+              query.Match(
+                query.Index('user_by_email'),
+                query.Casefold(user.email)
+              )
+            )
           )
         )
   
